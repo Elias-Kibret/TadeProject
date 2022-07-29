d1500efa30aed0edff5e2b2e1384c859cb57b06d
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-07-29 10:34:48 -0700

    filter

---
+++ frontend/pages/presentation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Index from '../Container/P/Index'
+const presentation = () => {
+  return (
+    <div>
+        <Index/>
+    </div>
+  )
+}
+
+export default presentation
