3df11dca476f82b992428c319893376b0d7f4030
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-03 14:03:32 -0700

    filter done

--- frontend/Container/Publications/List.js
+++ frontend/Container/Publications/List.js
@@ -8,24 +8,30 @@ const List = () => {
     const [display, setDisplay]=useState(false)
     const [publication,setPublications]=useState([]) 
     const [filtered_publication, setFiltered_publication]=useState([])
-    const year=[2022, 2021,2020,2019,2018,2017]
+    const year=[2022,2021,2020,2019,2018]
   
     useEffect(()=>{
        const publications='*[_type=="Publications"]'
        client.fetch(publications).then((data)=>{
         setPublications(data)
-        setFiltered_publication(publication)
+        setFiltered_publication(data)
        })
     },[])
      
    const filter=(year)=>{
-       
+       console.log(year)
        setFiltered_publication(publication.filter((item,index)=>{
-        return item.Year===year
+        if(year=="All"){
+          return publication
+        }
+        else{
+          return item.Year==year
+        }
+        
       }))
     
    }
-  
+  console.log(filtered_publication)
 
   return (
     <div>
@@ -34,45 +40,18 @@ const List = () => {
                <div><h3 className="text-[#FFFFFF] text-3xl  "><span className='text-[#2ecc71] font-semibold text-4xl '> {filtered_publication.length} </span> Publications </h3></div>
                <div className="flex items-center  text-[#FFFFFF] mx-14">
                 <button className="bg-[#2ecc71] px-4 py-2 mr-6  rounded-lg">Filter</button>
-                <div  className="flex items-center  cursor-pointer relative " 
-                      onMouseEnter={()=>{setDisplay(true)}}
-                      onMouseLeave={()=>{setDisplay(false)}}
-                      >
-                        <button href="/research" className="cursor-pointer ">
-                            <span className='px-6 flex items-center hover-state'>
-                     
-                            <a>
-
-                              Year
-                                      </a>
-                                    <BiCaretDown/>                
-
-                            </span>
-                        </button>
-                        {
-                          display&&(
-                            <div className='years w-[200px] bg-[black]  flex flex-col '
-                            onMouseEnter={()=>{setDisplay(true)}}
-                            onMouseLeave={()=>{setDisplay(false)}}
-                            >
-                              {
-                                year.map((item,index)=>(
-                                  <button ><a className='' onClick={()=>{
-                                    filter(item)
-                                  }}>{item}</a></button>
-                                ))
-                              }
-                          
-                           
-
-                        </div>
-
-          
-                          )
-                        }
-      
-
-
+                
+                <div>
+                  <select className='text-[black]  px-3 py-2' onChange={(e)=>{filter(e.target.value)}}>
+                    <option>All</option>
+                     {
+                      year.map((years,index)=>{
+                        return(
+                          <option className='h-10'>{years}</option>
+                        )
+                      })
+                     }
+                  </select>
                 </div>
                 
                </div>
