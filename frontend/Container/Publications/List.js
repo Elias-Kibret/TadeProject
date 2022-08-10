3667cdf9da6eb38fcaf0ddb72cf491da1ce642a1
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-10 01:23:28 -0700

    Toggle between Journal and Confrance

--- frontend/Container/Publications/List.js
+++ frontend/Container/Publications/List.js
@@ -7,6 +7,8 @@ import Link from 'next/link'
 
 const List = () => {
     const [display, setDisplay]=useState(false)
+    const [displayJournal, setDisplayJournal]=useState([true])
+    const [displayConfernace, setDisplayConference]=useState([true])
     const [publications,setPublications]=useState([])
     const [Journalpublications,SetJournalPublicatiopns]=useState([]) 
     const [conferencePublications,SetConferencePublications]=useState([])
@@ -41,18 +43,53 @@ const List = () => {
       }))
     
    }
+   const Toggle=(type)=>{
+       if(type==='Both')
+       {
+        setDisplayConference(true)
+        setDisplayJournal(true)
+       }
+       else if(type==='Journal')
+       {
+        setDisplayJournal(true)
+        setDisplayConference(false)
+       }
+        else if(type==="Conference")
+       {
+        setDisplayConference(true)
+        setDisplayJournal(false)
+       }
+       else{
+        setDisplayConference(true)
+        setDisplayPublications(true)
+       }
+   }
   console.log(filtered_publication)
 
   return (
     <div>
         <div>
-        <div className="flex md: my-16 lg:mx-24  items-center flex-col sm:flex-row">
+        <div className="flex md: my-16  w-full justify-center  items-center flex-col sm:flex-row">
                <div><h3 className="text-[#FFFFFF] text-3xl  "><span className='text-[#2ecc71] font-semibold text-4xl '> {filtered_publication.length} </span> Publications </h3></div>
-               <div className="flex items-center  text-[#FFFFFF] mx-14">
-                <button className="bg-[#2ecc71] px-4 py-2 mr-6  rounded-lg">Filter</button>
+               <div className="flex items-center flex-col md:flex-row justify-between   text-[#FFFFFF] sm:mx-14">
+                <div className="px-4 py-2 mr-6 font-semibold ">Filter by </div>
                 
-                <div>
-                  <select className='text-[black]  px-3 py-2' onChange={(e)=>{filter(e.target.value)}}>
+                <div className='flex justify-between'>
+                  <div className='mr-8'>
+
+               
+                  <select className='text-[black]  px-3 py-1 rounded-lg bg-[#2ecc71] text-xs sm:text-sm md:text-lg'  onChange={(e)=>{Toggle(e.target.value)}}>
+                    <option>Type</option>
+                    <option className=''>Both</option>
+                    <option>Journal</option>
+                    <option>Conference</option>
+                  </select>
+                  </div>
+                  <div className='md:mx-8'>
+
+               
+                  <select className='text-[black] rounded-lg  px-3 py-1 bg-[#2cce71] text-xs sm:text-sm md:text-lg' onChange={(e)=>{filter(e.target.value)}}>
+                    <option>Year</option>
                     <option>All</option>
                      {
                       year.map((years,index)=>{
@@ -62,26 +99,43 @@ const List = () => {
                       })
                      }
                   </select>
+                  </div>
                 </div>
                 
                </div>
             </div>
         </div>
         <div>
-        <div className='my-32'>
+    
+{
+  displayJournal&&(
+    <div>
+      <div className='my-32'>
 
-<h2 className="text-[#FFFFFF] text-3xl mt-8  font-[poppins] font-bold flex items-center"><FaMinus className='mr-3' />Peer-Reviewed Journal Publications
-</h2>    
-</div>
-          <Publication data={Journalpublications}/>
+      <h2 className="text-[#FFFFFF] text-3xl mt-8  font-[poppins] font-bold flex items-center"><FaMinus className='mr-3' />Peer-Reviewed Journal Publications
+      </h2>    
+      </div>
+      <Publication data={Journalpublications}/>
+    </div>
+  )
+}
+         
         </div>
-        <div className='my-32'>
+      
+{
+  displayConfernace&&(
+  <div>
+    <div className='my-32'>
 
 <h2 className="text-[#FFFFFF] text-3xl mt-8  font-[poppins] font-bold flex items-center"><FaMinus className='mr-3' />Conference Publications (Scopus indexed)
 
 </h2>    
 </div>
-       <Publication data={conferencePublications}/> 
+    <Publication data={conferencePublications}/>
+  </div>
+   )
+}
+       
        <p>Hello</p>
     </div>
   )
