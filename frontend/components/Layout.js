350469baee0918ea80becb716263bdf4c1c86ca3
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-17 04:18:50 -0700

    notification

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -3,6 +3,7 @@ import Header from "./Header"
 import Footer from "./Footer";
 const Layout = ({children}) => {
   const contactRef=useRef(null)
+  const success=new Notification()
   return (
     
       <div className="relative  overflow-hidden">
